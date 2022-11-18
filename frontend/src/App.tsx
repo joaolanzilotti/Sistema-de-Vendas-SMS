bf6c53e89e83308e4f3a08f2722a7e2b7a5d5af4
João Pedro Lanzilotti Ferretti <algamerjoao1@hotmail.com>
2022-11-18 15:06:39 -0300

    Outros Componentes em Progresso

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import NotificationButtton from "./components/notificationbutton"
+import Header from "./components/Header"
 
 function App() {
   return (
     <>
+    <Header />
     <h1>Comeco Do Site</h1>
     <NotificationButtton />
     </>
---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import logo from '../../assets/img/image-header.svg'
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="DSMeta" />
+                <h1>DSMeta</h1>
+                <p>
+                    Desenvolvido por
+                    <a href="https://github.com/joaosinho2"> JP Corporation</a>
+                </p>
+            </div>
+        </header>
+    )
+}
+export default Header
