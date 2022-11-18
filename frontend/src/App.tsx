cd7e02be5e27a5f7ccc7b18156bae8b04924664f
João Pedro Lanzilotti Ferretti <algamerjoao1@hotmail.com>
2022-11-18 14:37:12 -0300

    Meu Primeiro Componente

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,11 @@
+import NotificationButtton from "./components/notificationbutton"
 
 function App() {
   return (
+    <>
     <h1>Comeco Do Site</h1>
+    <NotificationButtton />
+    </>
   )
 }
 
---
+++ frontend/src/components/notificationbutton/index.tsx
@@ -0,0 +1,11 @@
+import icon from '../../assets/img/notification-icon.svg'
+import './styles.css'
+
+function NotificationButtton(){
+    return (
+        <div className="dsmeta-red-btn">
+                          <img src={icon} alt="Notificar" />
+                        </div>
+    )
+}
+export default NotificationButtton
