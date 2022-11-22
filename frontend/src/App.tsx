91e8ea0cf98bcf7ae37e94e750a4c60b55d42899
João Pedro Lanzilotti Ferretti <algamerjoao1@hotmail.com>
2022-11-22 09:50:13 -0300

    outros componentes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import NotificationButtton from "./components/notificationbutton"
 import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
 
 function App() {
   return (
     <>
     <Header />
     <h1>Comeco Do Site</h1>
-    <NotificationButtton />
+    <SalesCard />
     </>
   )
 }
