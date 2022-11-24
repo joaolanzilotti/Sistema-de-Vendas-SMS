1145229205b2ddc7aa44348439ea99940ec88054
João Pedro Lanzilotti Ferretti <algamerjoao1@hotmail.com>
2022-11-24 12:56:07 -0300

    Sale listing

---
+++ frontend/src/utils/request.ts
@@ -0,0 +1,3 @@
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? "http://localhost:8080";
+//import.meta.env.VITE_BACKEND_URL -> pega a variavel do hambiente
+//?? -> se nao conter a variavel do hambiente ele troca pro valor especific
