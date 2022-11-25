39d4b55c565af4e2f96f336414494145a29bad32
João Pedro Lanzilotti Ferretti <algamerjoao1@hotmail.com>
2022-11-25 13:54:23 -0300

    implementado spring backend como URL base

--- frontend/src/utils/request.ts
+++ frontend/src/utils/request.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? "https://dsmeta-joaop.herokuapp.com/sales";
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? "https://dsmeta-joaop.herokuapp.com";
 //import.meta.env.VITE_BACKEND_URL -> pega a variavel do hambiente
 //?? -> se nao conter a variavel do hambiente ele troca pro valor especific
