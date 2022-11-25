4b09eb222e7455567c79500b05817c9f0e368346
João Pedro Lanzilotti Ferretti <algamerjoao1@hotmail.com>
2022-11-25 12:02:44 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import NotificationButtton from "./components/notificationbutton";
 import Header from "./components/Header";
 import SalesCard from "./components/SalesCard";
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
   return (
     <>
+    <ToastContainer />
     <Header />
     <SalesCard />
     </>
--- frontend/src/components/notificationbutton/index.tsx
+++ frontend/src/components/notificationbutton/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import icon from '../../assets/img/notification-icon.svg';
 import { BASE_URL } from '../../utils/request';
 import './styles.css';
@@ -10,7 +11,7 @@ type Props = {
 function handleClick(saleId: number){
     axios(`${BASE_URL}/sales/${saleId}/notification`)
     .then(Response =>{
-        console.log('SUCESSO!');
+        toast.info("SMS Enviado com Sucesso");
     });
 }
 
