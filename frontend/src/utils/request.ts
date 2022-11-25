1061aeeb035a05235606c5144ab79bc1c0fc8378
João Pedro Lanzilotti Ferretti <algamerjoao1@hotmail.com>
2022-11-25 13:52:36 -0300

    testando

--- frontend/src/utils/request.ts
+++ frontend/src/utils/request.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? "https://dsmeta-joaop.herokuapp.com";
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? "https://dsmeta-joaop.herokuapp.com/sales";
 //import.meta.env.VITE_BACKEND_URL -> pega a variavel do hambiente
 //?? -> se nao conter a variavel do hambiente ele troca pro valor especific
